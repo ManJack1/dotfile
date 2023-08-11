d621dfa1f71f8b478f898bb57d197db5b0c95448
ManJack <2284414969@qq.com>
2023-08-11 18:14:28 +0800

    add some color scheme

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -3,8 +3,4 @@ return {
   config = function()
     require("colorizer").setup()
   end,
-  keys = {
-    { "L", "<cmd>CompilerOpen<cr>", desc = "CompilerOpen" },
-    { "T", "<cmd>CompilerToggleResults<cr>", desc = "CompilerToggleResults" },
-  },
 }
